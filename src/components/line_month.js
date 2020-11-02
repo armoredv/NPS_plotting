350eb898761343ceb23e6cb7e9927ff8046832c4
NEELANSH VERMA <vermaneelansh2001@iitkgp.ac.in>
2020-11-02 16:10:32 +0530

    Update line_month.js

--- src/components/line_month.js
+++ src/components/line_month.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { ResponsiveLine } from "@nivo/line";
 import data from "../data1";
-
-// make sure parent container have a defined height when using responsive component,
-// otherwise height will be 0 and no chart will be rendered.
-// website examples showcase many properties, you'll often use just a few of them.
 const Line_month = () => (
   <ResponsiveLine
     data={data}
